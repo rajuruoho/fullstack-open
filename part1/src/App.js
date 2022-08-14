1952b572eca98a91298c48e001514402f1b82cf5
drjaska <drjaska83@gmail.com>
2022-08-14 12:04:55 +0300

    1.12

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>
+}
+
 const StatisticLine = (props) => {
   return (
     <tr>
@@ -50,14 +54,20 @@ const Statistics = (props) => {
   }
 }
 
-const Button = (props) => {
-  return <button onClick={props.handleClick}>{props.text}</button>
-}
-
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [selectedAnecdote, setAnecdote] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
+  ]
 
   const handleGoodClick = () => {
     setGood(good + 1)
@@ -71,6 +81,10 @@ const App = () => {
     setBad(bad + 1)
   }
 
+  const handleRandomAnecdote = () => {
+    setAnecdote(Math.floor(Math.random() * (anecdotes.length)))
+  }
+
   return (
     <div>
         <h1>give feedback</h1>
@@ -78,6 +92,9 @@ const App = () => {
         <Button handleClick={() => handleNeutralClick()} text="neutral" />
         <Button handleClick={() => handleBadClick()} text="bad" />
         <Statistics good={good} neutral={neutral} bad={bad} />
+        <h2>Ahecdote of the day</h2>
+	<p>{anecdotes[selectedAnecdote]}</p>
+        <Button handleClick={() => handleRandomAnecdote()} text="random anecdote" />
     </div>
   )
 }
