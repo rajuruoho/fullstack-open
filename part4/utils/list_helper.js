e17fb735fb34627cc1d01576e5bd678a3d5dea1c
rajuruoho <rauli.j.ruokokoski@student.jyu.fi>
2023-07-12 11:49:48 +0300

    4.3-4.5 done

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,29 @@
 const dummy = (blogs) => {
-  // ...
+  return 1;
 }
 
+const totalLikes = (blogs) => {
+  let totalLikes = 0;
+  for (let i = 0; i < blogs.length; i++) {
+    totalLikes += blogs[i].likes;
+  }
+  return totalLikes;
+};
+
+const favoriteBlog = (blogs) => {
+  let favorite = blogs[0];
+
+  if (blogs.length === 1) return;
+
+  for (let i = 1; i < blogs.length; i++) {
+    if (blogs[i].likes > favorite.likes)
+    favorite = blogs[i];
+  }
+  return favorite;
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog,
 }
